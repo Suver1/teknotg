959514cac1c882e44a9fe3f4a9c49d8ecfe4c4bc
Andreas Hellesvik <andreashellesvik@hotmail.com>
2015-04-01 11:48:02 -0400

    added level01.js.

---
+++ src/level01.js
@@ -0,0 +1,24 @@
+var Level01 = function(game) {
+    // insert global variables here
+    score = 0;
+}
+
+Level01.prototype = {
+    create: function() {
+        console.log("Level 01");
+        score = 1;
+        var gameOver = this.game.add.button(this.game.width / 2, this.game.height / 2, "star", this.gameOverScreen, this);
+        player = this.game.add.sprite(150, 150, "player1");
+    },
+    update: function() {
+        // runs every frame. insert game logic here.
+    },
+    gameOverScreen: function() {
+        // this.game.state.start parameters: 
+        // 1. Name of the state to start,
+        // 2. clearWorld (default true) clears the World display list fully (but not the Stage, so if you have added your own objects to the Stage they will need managing directly).
+        // 3. clearCache (default false) clears all loaded assets.
+        // 4. All other parameters from the fourth are variables that will be passed to the init function (if it has one). We pass the score to the GameOver state.
+        this.game.state.start("GameOver", true, false, score);
+    }
+}
