9cc2a3d616c44feb38743db4d756e94b82120095
Andreas Hellesvik <andreashellesvik@hotmail.com>
2015-04-01 12:47:34 -0400

    added basic gravity and collision between player and ground.

--- src/preload.js
+++ src/preload.js
@@ -7,6 +7,7 @@ Preload.prototype = {
         // load loading bar
         // load assets
         this.game.load.image("star", "assets/star.png");
+        this.game.load.image("platform", "assets/platform.png");
         this.game.load.atlasJSONHash("player1", "assets/pgmanSprite.png", "assets/pgman.json");
 
     },
