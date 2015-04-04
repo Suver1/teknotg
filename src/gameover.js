cf18d82cfceaa25ed6f2398039e8f9dd84baa897
Ole Strøm <ole@railway.no>
2015-04-04 11:29:24 +0200

    fixed gameover

--- src/gameover.js
+++ src/gameover.js
@@ -4,20 +4,30 @@ var GameOver = function(game) {
 
 GameOver.prototype = {
     create: function(game, currentScore) {
+
         var bg = this.game.add.sprite(0, 0, 'background');
-            bg.fixedToCamera = true;
-        style = {fontSize: '32px', fill: '#000'};
-        game.add.text(16, 16, 'Congratulations!\nYou made it in ' + this.game.scoreManager.currentTime / 1000 + ' seconds', style);
-        //game.add.text(16, 16, 'time: ' + this.game.scoreManager.currentTime / 1000 + ' sec ', style);
-        // place game over stuff here
-        var flag = this.game.add.button(this.game.width / 2, this.game.height / 2, "flag", this.playTheGame, this);
+
+        bg.fixedToCamera = true;
+
+        style = {fontSize: '32px', fill: '#FFFFFF'};
+
+        game.add.text(this.game.width / 4, 100,
+                      'Congratulations!\nYou made it in ' + this.game.scoreManager.currentTime / 1000 + ' seconds',
+                      style);
+
+        setTimeout(function() {
+            this.game.state.start("StartMenu");
+
+            this.game.backgroundMusic.stop();
+
+        }, 3000);
+
     },
+
     render: function() {
         if (this.game.debugMode) {
             this.game.debug.text('fps: ' + this.game.time.fps, 0, 17, '#00FF00');
         }
-    },
-    playTheGame: function() {
-        this.game.state.start("Level01");
     }
+
 };
