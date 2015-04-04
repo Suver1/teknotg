92e3d0b522ad70017cf20cbc45fc37c55dd99473
Ole Strøm <ole@railway.no>
2015-04-04 11:00:20 +0200

    removed logging from gameover

--- src/gameover.js
+++ src/gameover.js
@@ -9,7 +9,6 @@ GameOver.prototype = {
         style = {fontSize: '32px', fill: '#000'};
         game.add.text(16, 16, 'Congratulations!\nYou made it in ' + this.game.scoreManager.currentTime / 1000 + ' seconds', style);
         //game.add.text(16, 16, 'time: ' + this.game.scoreManager.currentTime / 1000 + ' sec ', style);
-        console.log("gameOver");
         // place game over stuff here
         var flag = this.game.add.button(this.game.width / 2, this.game.height / 2, "flag", this.playTheGame, this);
     },
